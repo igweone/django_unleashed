07e2ff7bd4b7630f05ea90e54e970af9de87dd09
Emeka Osigwe <emeka@intelligenti.io>
2019-03-05 10:57:55 +0100

    completed chapter4: Templates and Rendering

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.template import Context, loader
 
-# Create your views here.
+from .models import Tag
+
+
+def homepage(request):
+    tag_list = Tag.objects.all()
+    template = loader.get_template(
+        'organizer/tag_list.html')
+    context = Context({'tag_list': tag_list})
+    output = template.render(context)
+    return HttpResponse(output)
+
+    # return HttpResponse(loader.get_template('organizer/tag_list.html').render(Context({'tag_list':Tag.objects.all()})))
